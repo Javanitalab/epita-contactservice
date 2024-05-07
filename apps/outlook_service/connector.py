a1ddb3125b9eea0a85d72bd7f5a2260c414c6ca0
Croissant (Mohammad) <118067995+Javanitalab@users.noreply.github.com>
2024-05-07 19:17:11 +0200

    init structure of contact service

---
+++ apps/outlook_service/connector.py
@@ -0,0 +1,20 @@
+import requests
+
+
+class OutlookServiceConnector():
+
+    def send_ping(self, endpoint : str):
+
+        response = requests.get(url=endpoint)
+
+        if response.status_code == 200:
+            return True
+        else:
+            return False
+
+
+    def create_contact(self,endpoint : str , data : dict):
+
+        response = requests.post(url=endpoint,data=data)
+
+        return response
