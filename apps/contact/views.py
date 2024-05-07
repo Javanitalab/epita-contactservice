3aa82d71f8e356b1ed4938fcb9e378cc9cac85e5
Croissant (Mohammad) <118067995+Javanitalab@users.noreply.github.com>
2024-05-07 19:45:52 +0200

    creating contact model/view/serializer

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import datetime
+
+from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ViewSet
+
+from apps.contact.models import Contact
+from apps.contact.serializers import ContactSerializer
+
 
 # Create your views here.
+
+
+class ContactViewSet(ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
